495d055091150b68c39e2b3f0ed4fa54b0336d01
Jesse Russell <herewegojes1@gmail.com>
2017-10-16 14:29:11 -0400

    Partially implemented the placement file and fixed some buildings

--- Assets/Scripts/Instantiate.cs
+++ Assets/Scripts/Instantiate.cs
@@ -12,7 +12,7 @@ public class Instantiate : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        string[] placementFile = File.ReadAllLines("Assets/Resources/placement.txt");
+        string[] placementFile = File.ReadAllLines("Assets/Resources/placementOrigin.txt");
         for (int i = 0; i < placementFile.Length; i++)
         {
             string holder = placementFile[i];
@@ -24,6 +24,7 @@ public class Instantiate : MonoBehaviour
             Match m = r.Match(holder);
             if (!m.Success)
             {
+				Debug.Log("Did not match regex: " + holder);
                 continue;
             }
 
@@ -34,18 +35,19 @@ public class Instantiate : MonoBehaviour
             GameObject prefab = Resources.Load(path) as GameObject;
             if (prefab == null)
             {
+				Debug.Log("Couldn't find prefab: " + path);
                 continue;
             }
             GameObject placedThing = Instantiate(prefab);
 
             placedThing.name = name;
 
-            float x = float.Parse(g[3].Value) * -2.0f;
+            float x = float.Parse(g[3].Value) ;//* -2.0f;
             float y = float.Parse(g[4].Value);
-            float z = float.Parse(g[5].Value) * 2.0f;
+            float z = float.Parse(g[5].Value);// * 2.0f;
             Vector3 location = new Vector3(x, y, z);
 
-            float s = float.Parse(g[6].Value) * 1.75f;
+            float s = float.Parse(g[6].Value);// * 1.75f;
             Vector3 scale = new Vector3(s, s, s);
 
             float qx = float.Parse(g[7].Value);
