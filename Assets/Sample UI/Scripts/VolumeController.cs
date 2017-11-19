ef77754622de7f6fdef3981534f1588e835034b0
Pikapal101 <annie.pikapal@gmail.com>
2017-11-19 11:29:33 -0500

    master vol!

---
+++ Assets/Sample UI/Scripts/VolumeController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeController : MonoBehaviour {
+
+	public void masterVolume(float f){
+		AudioListener.volume = f;
+	}
+
+}
