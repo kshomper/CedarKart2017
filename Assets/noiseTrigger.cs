37bb0ff6f876c7cdfb208bb6aed7748929a086e7
Unknown <kedarst@cedarville.edu>
2017-11-30 20:49:34 -0500

    added noise to most buildings
    bridges, lampost prefab, ssc clock, watertower, an dbuildings (except for epl & callen) all have collision noises. epl is go thro so I donno whats going on. callen has tiny spot so that the collision mesh covers a much larger area so I didnt put sound on that. also has random collision noise at the beginning and I'm still trying to figure out why.

---
+++ Assets/noiseTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class noiseTrigger : MonoBehaviour {
+
+    public AudioClip clang;
+    private AudioSource audiosource;
+
+
+    void Awake()
+    {
+        audiosource = GetComponent<AudioSource>();
+    }
+
+
+    private void OnTriggerEnter()
+    {
+        audiosource.PlayOneShot(clang, 1f);
+    }
+}
