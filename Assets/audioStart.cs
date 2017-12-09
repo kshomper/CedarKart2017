fff2dc232ecc7c25fc5ac4f920cade87b98052b2
Kirby <kedarst@cedarville.edu>
2017-12-09 01:07:24 -0500

    made a moment of silence

---
+++ Assets/audioStart.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class audioStart : MonoBehaviour {
+	
+	public AudioMixer music;
+	public AudioMixer background;
+	float startTime;
+	bool startAudio;
+	// Use this for initialization
+	void Start () {
+		background.SetFloat ("BackVol", -80f);
+		music.SetFloat ("MusicVol", -80f);
+		startTime = Time.time + 5f;
+		startAudio = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (startTime < Time.time) {
+			startAudio = false;
+		}
+		if (!startAudio) {
+			background.SetFloat ("BackVol", 0);
+			music.SetFloat ("MusicVol", 0);
+		}
+	}
+}
